2e4ee95957aa2831a9bf3a88fdcf9fab07c708e8
Eugene <eugenewickett@gmail.com>
2023-09-07 19:09:52 -0500

    op samp plan update

--- operationalizedsamplingplans.py
+++ operationalizedsamplingplans.py
@@ -17,3 +17,202 @@ from numpy.random import choice
 import random
 import scipy.stats as sps
 import scipy.special as spsp
+
+# Pull data from analysis of first paper
+def GetSenegalDataMatrices(deidentify=False):
+    # Pull Senegal data from MQDB
+    SCRIPT_DIR = os.getcwd()
+    filesPath = os.path.join(SCRIPT_DIR, 'MQDfiles')
+    outputFileName = os.path.join(filesPath, 'pickleOutput')
+    openFile = open(outputFileName, 'rb')  # Read the file
+    dataDict = pickle.load(openFile)
+
+    SEN_df = dataDict['df_SEN']
+    # 7 unique Province_Name_GROUPED; 23 unique Facility_Location_GROUPED; 66 unique Facility_Name_GROUPED
+    # Remove 'Missing' and 'Unknown' labels
+    SEN_df_2010 = SEN_df[(SEN_df['Date_Received'] == '7/12/2010') & (SEN_df['Manufacturer_GROUPED'] != 'Unknown') & (
+                SEN_df['Facility_Location_GROUPED'] != 'Missing')].copy()
+    tbl_SEN_G1_2010 = SEN_df_2010[['Province_Name_GROUPED', 'Manufacturer_GROUPED', 'Final_Test_Conclusion']].values.tolist()
+    tbl_SEN_G1_2010 = [[i[0], i[1], 1] if i[2] == 'Fail' else [i[0], i[1], 0] for i in tbl_SEN_G1_2010]
+    tbl_SEN_G2_2010 = SEN_df_2010[['Facility_Location_GROUPED', 'Manufacturer_GROUPED', 'Final_Test_Conclusion']].values.tolist()
+    tbl_SEN_G2_2010 = [[i[0], i[1], 1] if i[2] == 'Fail' else [i[0], i[1], 0] for i in tbl_SEN_G2_2010]
+
+    SEN_df_2010.pivot_table(index=['Manufacturer_GROUPED'], columns=['Final_Test_Conclusion'],
+                            aggfunc='size', fill_value=0)
+    SEN_df_2010.pivot_table(index=['Province_Name_GROUPED'], columns=['Final_Test_Conclusion'],
+                            aggfunc='size', fill_value=0)
+    SEN_df_2010.pivot_table(index=['Facility_Location_GROUPED'], columns=['Final_Test_Conclusion'],
+                            aggfunc='size', fill_value=0)
+    pivoted = SEN_df_2010.pivot_table(index=['Facility_Name_GROUPED'], columns=['Final_Test_Conclusion'],
+                                      aggfunc='size', fill_value=0)
+    # pivoted[:15]
+    # SEN_df_2010['Province_Name_GROUPED'].unique()
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Dakar', 'Kaffrine', 'Kedougou', 'Kaolack'])].pivot_table(
+        index=['Manufacturer_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Matam', 'Kolda', 'Saint Louis'])].pivot_table(
+        index=['Manufacturer_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Dakar', 'Kaffrine', 'Kedougou', 'Kaolack']) & SEN_df_2010[
+        'Final_Test_Conclusion'].isin(['Fail'])].pivot_table(
+        index=['Manufacturer_GROUPED'], columns=['Province_Name_GROUPED', 'Final_Test_Conclusion'],
+        aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Matam', 'Kolda', 'Saint Louis']) & SEN_df_2010[
+        'Final_Test_Conclusion'].isin(['Fail'])].pivot_table(
+        index=['Manufacturer_GROUPED'], columns=['Province_Name_GROUPED', 'Final_Test_Conclusion'],
+        aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Dakar', 'Kaffrine', 'Kedougou', 'Kaolack'])].pivot_table(
+        index=['Facility_Location_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Matam', 'Kolda', 'Saint Louis'])].pivot_table(
+        index=['Facility_Location_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Dakar', 'Kaffrine', 'Kedougou', 'Kaolack'])].pivot_table(
+        index=['Facility_Name_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Dakar', 'Kaffrine'])].pivot_table(
+        index=['Facility_Name_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Matam', 'Kolda', 'Saint Louis'])].pivot_table(
+        index=['Facility_Name_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Facility_Name_GROUPED'].isin(['Hopitale Regionale de Koda',
+                                                           "Pharmacie Keneya"])].pivot_table(
+        index=['Facility_Location_GROUPED'], columns=['Facility_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Dakar'])].pivot_table(
+        index=['Facility_Location_GROUPED'], columns=['Final_Test_Conclusion'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Facility_Location_GROUPED'].isin(['Tambacounda'])].pivot_table(
+        index=['Manufacturer_GROUPED'], columns=['Final_Test_Conclusion'], aggfunc='size', fill_value=0)
+
+    SEN_df_2010['Facility_Location_GROUPED'].count()
+
+    orig_MANUF_lst = ['Ajanta Pharma Limited', 'Aurobindo Pharmaceuticals Ltd', 'Bliss Gvis Pharma Ltd', 'Cipla Ltd',
+                      'Cupin', 'EGR pharm Ltd', 'El Nasr', 'Emcure Pharmaceuticals Ltd', 'Expharm',
+                      'F.Hoffmann-La Roche Ltd', 'Gracure Pharma Ltd', 'Hetdero Drugs Limited', 'Imex Health',
+                      'Innothera Chouzy', 'Ipca Laboratories', 'Lupin Limited', 'Macleods Pharmaceuticals Ltd',
+                      'Matrix Laboratories Limited', 'Medico Remedies Pvt Ltd', 'Mepha Ltd', 'Novartis', 'Odypharm Ltd',
+                      'Pfizer', 'Sanofi Aventis', 'Sanofi Synthelabo']
+    orig_PROV_lst = ['Dakar', 'Kaffrine', 'Kaolack', 'Kedougou', 'Kolda', 'Matam', 'Saint Louis']
+    orig_LOCAT_lst = ['Dioum', 'Diourbel', 'Fann- Dakar', 'Guediawaye', 'Hann', 'Kaffrine (City)', 'Kanel',
+                      'Kaolack (City)', 'Kebemer', 'Kedougou (City)', 'Kolda (City)', 'Koumpantoum', 'Matam (City)',
+                      'Mbour-Thies', 'Medina', 'Ouro-Sogui', 'Richard Toll', 'Rufisque-Dakar', 'Saint Louis (City)',
+                      'Tambacounda', 'Thies', 'Tivaoune', 'Velingara']
+    # DEIDENTIFICATION
+    if deidentify == True:
+        # Replace Manufacturers
+        shuf_MANUF_lst = orig_MANUF_lst.copy()
+        random.seed(333)
+        random.shuffle(shuf_MANUF_lst)
+        # print(shuf_MANUF_lst)
+        for i in range(len(shuf_MANUF_lst)):
+            currName = shuf_MANUF_lst[i]
+            newName = 'Mnfr. ' + str(i + 1)
+            for ind, item in enumerate(tbl_SEN_G1_2010):
+                if item[1] == currName:
+                    tbl_SEN_G1_2010[ind][1] = newName
+            for ind, item in enumerate(tbl_SEN_G2_2010):
+                if item[1] == currName:
+                    tbl_SEN_G2_2010[ind][1] = newName
+        # Replace Province
+        shuf_PROV_lst = orig_PROV_lst.copy()
+        random.seed(333)
+        random.shuffle(shuf_PROV_lst)
+        # print(shuf_PROV_lst)
+        for i in range(len(shuf_PROV_lst)):
+            currName = shuf_PROV_lst[i]
+            newName = 'Province ' + str(i + 1)
+            for ind, item in enumerate(tbl_SEN_G1_2010):
+                if item[0] == currName:
+                    tbl_SEN_G1_2010[ind][0] = newName
+        # Replace Facility Location
+        shuf_LOCAT_lst = orig_LOCAT_lst.copy()
+        random.seed(333)
+        random.shuffle(shuf_LOCAT_lst)
+        # print(shuf_LOCAT_lst)
+        for i in range(len(shuf_LOCAT_lst)):
+            currName = shuf_LOCAT_lst[i]
+            newName = 'District ' + str(i + 1)
+            for ind, item in enumerate(tbl_SEN_G2_2010):
+                if item[0] == currName:
+                    tbl_SEN_G2_2010[ind][0] = newName
+        # Swap Districts 7 & 8
+        for ind, item in enumerate(tbl_SEN_G2_2010):
+            if item[0] == 'District 7':
+                tbl_SEN_G2_2010[ind][0] = 'District 8'
+            elif item[0] == 'District 8':
+                tbl_SEN_G2_2010[ind][0] = 'District 7'
+
+    # Now form data dictionary
+    retDict = util.testresultsfiletotable(tbl_SEN_G2_2010, csvName=False)
+    if deidentify == True:
+        retlist_MANUF = shuf_MANUF_lst.copy()
+        retlist_PROV = shuf_PROV_lst.copy()
+        retlist_LOCAT = shuf_LOCAT_lst.copy()
+    else:
+        retlist_MANUF = orig_MANUF_lst.copy()
+        retlist_PROV = orig_PROV_lst.copy()
+        retlist_LOCAT = orig_LOCAT_lst.copy()
+
+    return retDict['N'], retDict['Y'], retlist_MANUF, retlist_PROV, retlist_LOCAT
+
+
+# Pull data from newly constructed CSV files
+def GetSenegalCSVData():
+    return 1+1
+
+# Pull district-level Senegal data
+N, Y, SNnames, TNprovs, TNnames = GetSenegalDataMatrices(deidentify=False)
+(numTN, numSN) = N.shape # For later use
+### Print some data summaries
+# Overall data
+print('TNs by SNs: ' + str(N.shape) + '\nNumber of Obsvns: ' + str(N.sum()) + '\nNumber of SFPs: ' + str(Y.sum()) + '\nSFP rate: ' + str(round(
+    Y.sum() / N.sum(), 4)))
+# TN-specific data
+print('Tests at TNs: ' + str(np.sum(N, axis=1)) + '\nSFPs at TNs: ' + str(np.sum(Y, axis=1)) + '\nSFP rates: ' + str(
+    (np.sum(Y, axis=1) / np.sum(N, axis=1)).round(4)))
+# Set up logistigate dictionary
+lgdict = util.initDataDict(N, Y)
+lgdict.update({'TNnames':TNnames, 'SNnames':SNnames})
+
+
+# TODO: INSPECT CHOICE HERE
+# All SNs are `Moderate'
+SNpriorMean = np.repeat(spsp.logit(0.1), numSN)
+# TNs are randomly assigned risk, such that 5% are in the 1st and 7th levels, 10% are in the 2nd and 6th levels,
+#   20% are in the 3rd and 5th levels, and 30% are in the 4th level
+np.random.seed(15)
+tempCategs = np.random.multinomial(n=1, pvals=[0.05,0.1,0.2,0.3,0.2,0.1,0.05], size=numTN)
+riskMeans = [0.01,0.02,0.05,0.1,0.15,0.2,0.25]
+randriskinds = np.mod(np.where(tempCategs.flatten()==1), len(riskMeans))[0]
+TNpriorMean = spsp.logit(np.array([riskMeans[randriskinds[i]] for i in range(numTN)]))
+# Concatenate prior means
+priorMean = np.concatenate((SNpriorMean, TNpriorMean))
+TNvar, SNvar = 2., 2.  # Variances for use with prior; supply nodes are wide due to large
+priorCovar = np.diag(np.concatenate((np.repeat(SNvar, numSN), np.repeat(TNvar, numTN))))
+priorObj = prior_normal_assort(priorMean, priorCovar)
+lgdict['prior'] = priorObj
+
+# Set up MCMC
+lgdict['MCMCdict'] = {'MCMCtype': 'NUTS', 'Madapt': 5000, 'delta': 0.4}
+# TODO: INSPECT CHOICE HERE
+numdraws = 20000
+lgdict['numPostSamples'] = numdraws
+np.random.seed(300)
+lgdict = methods.GeneratePostSamples(lgdict)
+# Print inference from initial data
+# util.plotPostSamples(lgdict, 'int90')
+print(lgdict['TNnames'])
+
+print(np.sum(lgdict['N'],axis=1))
+
+# Loss specification
+paramdict = lf.build_diffscore_checkrisk_dict(scoreunderestwt=5., riskthreshold=0.15, riskslope=0.6,
+                                              marketvec=np.ones(numTN + numSN))
+
+# Set limits of data collection and intervals for calculation
+testmax, testint = 400, 10
+testarr = np.arange(testint, testmax + testint, testint)
+
+# Set MCMC draws to use in fast algorithm
+numtruthdraws, numdatadraws = 10000, 500
+# Get random subsets for truth and data draws
+np.random.seed(444)
+truthdraws, datadraws = util.distribute_truthdata_draws(csdict_fam['postSamples'], numtruthdraws, numdatadraws)
+paramdict.update({'truthdraws': truthdraws, 'datadraws': datadraws})
+# Get base loss
+paramdict['baseloss'] = sampf.baseloss(paramdict['truthdraws'], paramdict)
+
+util.print_param_checks(paramdict)  # Check of used parameters
