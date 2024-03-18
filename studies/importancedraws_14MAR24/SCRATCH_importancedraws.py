50d4b45b8b9adcaa3e202c06899a841d44032e6f
Eugene <eugenewickett@gmail.com>
2024-03-18 16:53:12 -0500

    importance sampling scratch

---
+++ studies/importancedraws_14MAR24/SCRATCH_importancedraws.py
@@ -0,0 +1,29 @@
+from logistigate.logistigate import utilities as util # Pull from the submodule "develop" branch
+from logistigate.logistigate import methods
+from logistigate.logistigate.priors import prior_normal_assort
+from logistigate.logistigate import lossfunctions as lf
+from logistigate.logistigate import samplingplanfunctions as sampf
+from logistigate.logistigate import orienteering as opf
+
+import os
+import pickle
+import time
+
+import matplotlib
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+plt.rcParams["mathtext.fontset"] = "dejavuserif"
+plt.rcParams["font.family"] = "serif"
+import matplotlib.ticker as mtick
+
+import pandas as pd
+import numpy as np
+from numpy.random import choice
+import random
+import itertools
+import scipy.stats as sps
+import scipy.special as spsp
+
+import scipy.optimize as spo
+from scipy.optimize import LinearConstraint
+from scipy.optimize import milp
