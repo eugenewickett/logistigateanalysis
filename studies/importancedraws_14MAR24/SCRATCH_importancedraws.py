6897ac048f1ca6a6fa1a6cf71dd4ba0765501556
Eugene <eugenewickett@gmail.com>
2024-03-18 20:01:16 -0500

    importance sampling scratch

--- studies/importancedraws_14MAR24/SCRATCH_importancedraws.py
+++ studies/importancedraws_14MAR24/SCRATCH_importancedraws.py
@@ -27,3 +27,352 @@ import scipy.special as spsp
 import scipy.optimize as spo
 from scipy.optimize import LinearConstraint
 from scipy.optimize import milp
+
+# Pull data from analysis of first paper
+def GetSenegalDataMatrices(deidentify=False):
+    # Pull Senegal data from MQDB
+    SCRIPT_DIR = os.getcwd()
+    filesPath = os.path.join(SCRIPT_DIR, 'MQDfiles')
+    outputFileName = os.path.join(filesPath, 'pickleOutput')
+    openFile = open(outputFileName, 'rb')  # Read the file
+    dataDict = pickle.load(openFile)
+
+    SEN_df = dataDict['df_SEN']
+    # 7 unique Province_Name_GROUPED; 23 unique Facility_Location_GROUPED; 66 unique Facility_Name_GROUPED
+    # Remove 'Missing' and 'Unknown' labels
+    SEN_df_2010 = SEN_df[(SEN_df['Date_Received'] == '7/12/2010') & (SEN_df['Manufacturer_GROUPED'] != 'Unknown') & (
+                SEN_df['Facility_Location_GROUPED'] != 'Missing')].copy()
+    tbl_SEN_G1_2010 = SEN_df_2010[['Province_Name_GROUPED', 'Manufacturer_GROUPED', 'Final_Test_Conclusion']].values.tolist()
+    tbl_SEN_G1_2010 = [[i[0], i[1], 1] if i[2] == 'Fail' else [i[0], i[1], 0] for i in tbl_SEN_G1_2010]
+    tbl_SEN_G2_2010 = SEN_df_2010[['Facility_Location_GROUPED', 'Manufacturer_GROUPED', 'Final_Test_Conclusion']].values.tolist()
+    tbl_SEN_G2_2010 = [[i[0], i[1], 1] if i[2] == 'Fail' else [i[0], i[1], 0] for i in tbl_SEN_G2_2010]
+
+    SEN_df_2010.pivot_table(index=['Manufacturer_GROUPED'], columns=['Final_Test_Conclusion'],
+                            aggfunc='size', fill_value=0)
+    SEN_df_2010.pivot_table(index=['Province_Name_GROUPED'], columns=['Final_Test_Conclusion'],
+                            aggfunc='size', fill_value=0)
+    SEN_df_2010.pivot_table(index=['Facility_Location_GROUPED'], columns=['Final_Test_Conclusion'],
+                            aggfunc='size', fill_value=0)
+    pivoted = SEN_df_2010.pivot_table(index=['Facility_Name_GROUPED'], columns=['Final_Test_Conclusion'],
+                                      aggfunc='size', fill_value=0)
+    # pivoted[:15]
+    # SEN_df_2010['Province_Name_GROUPED'].unique()
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Dakar', 'Kaffrine', 'Kedougou', 'Kaolack'])].pivot_table(
+        index=['Manufacturer_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Matam', 'Kolda', 'Saint Louis'])].pivot_table(
+        index=['Manufacturer_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Dakar', 'Kaffrine', 'Kedougou', 'Kaolack']) & SEN_df_2010[
+        'Final_Test_Conclusion'].isin(['Fail'])].pivot_table(
+        index=['Manufacturer_GROUPED'], columns=['Province_Name_GROUPED', 'Final_Test_Conclusion'],
+        aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Matam', 'Kolda', 'Saint Louis']) & SEN_df_2010[
+        'Final_Test_Conclusion'].isin(['Fail'])].pivot_table(
+        index=['Manufacturer_GROUPED'], columns=['Province_Name_GROUPED', 'Final_Test_Conclusion'],
+        aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Dakar', 'Kaffrine', 'Kedougou', 'Kaolack'])].pivot_table(
+        index=['Facility_Location_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Matam', 'Kolda', 'Saint Louis'])].pivot_table(
+        index=['Facility_Location_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Dakar', 'Kaffrine', 'Kedougou', 'Kaolack'])].pivot_table(
+        index=['Facility_Name_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Dakar', 'Kaffrine'])].pivot_table(
+        index=['Facility_Name_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Matam', 'Kolda', 'Saint Louis'])].pivot_table(
+        index=['Facility_Name_GROUPED'], columns=['Province_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Facility_Name_GROUPED'].isin(['Hopitale Regionale de Koda',
+                                                           "Pharmacie Keneya"])].pivot_table(
+        index=['Facility_Location_GROUPED'], columns=['Facility_Name_GROUPED'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Province_Name_GROUPED'].isin(['Dakar'])].pivot_table(
+        index=['Facility_Location_GROUPED'], columns=['Final_Test_Conclusion'], aggfunc='size', fill_value=0)
+    SEN_df_2010[SEN_df_2010['Facility_Location_GROUPED'].isin(['Tambacounda'])].pivot_table(
+        index=['Manufacturer_GROUPED'], columns=['Final_Test_Conclusion'], aggfunc='size', fill_value=0)
+
+    SEN_df_2010['Facility_Location_GROUPED'].count()
+
+    orig_MANUF_lst = ['Ajanta Pharma Limited', 'Aurobindo Pharmaceuticals Ltd', 'Bliss Gvis Pharma Ltd', 'Cipla Ltd',
+                      'Cupin', 'EGR pharm Ltd', 'El Nasr', 'Emcure Pharmaceuticals Ltd', 'Expharm',
+                      'F.Hoffmann-La Roche Ltd', 'Gracure Pharma Ltd', 'Hetdero Drugs Limited', 'Imex Health',
+                      'Innothera Chouzy', 'Ipca Laboratories', 'Lupin Limited', 'Macleods Pharmaceuticals Ltd',
+                      'Matrix Laboratories Limited', 'Medico Remedies Pvt Ltd', 'Mepha Ltd', 'Novartis', 'Odypharm Ltd',
+                      'Pfizer', 'Sanofi Aventis', 'Sanofi Synthelabo']
+    orig_PROV_lst = ['Dakar', 'Kaffrine', 'Kaolack', 'Kedougou', 'Kolda', 'Matam', 'Saint Louis']
+    orig_LOCAT_lst = ['Dioum', 'Diourbel', 'Fann- Dakar', 'Guediawaye', 'Hann', 'Kaffrine (City)', 'Kanel',
+                      'Kaolack (City)', 'Kebemer', 'Kedougou (City)', 'Kolda (City)', 'Koumpantoum', 'Matam (City)',
+                      'Mbour-Thies', 'Medina', 'Ouro-Sogui', 'Richard Toll', 'Rufisque-Dakar', 'Saint Louis (City)',
+                      'Tambacounda', 'Thies', 'Tivaoune', 'Velingara']
+    # DEIDENTIFICATION
+    if deidentify == True:
+        # Replace Manufacturers
+        shuf_MANUF_lst = orig_MANUF_lst.copy()
+        random.seed(333)
+        random.shuffle(shuf_MANUF_lst)
+        # print(shuf_MANUF_lst)
+        for i in range(len(shuf_MANUF_lst)):
+            currName = shuf_MANUF_lst[i]
+            newName = 'Mnfr. ' + str(i + 1)
+            for ind, item in enumerate(tbl_SEN_G1_2010):
+                if item[1] == currName:
+                    tbl_SEN_G1_2010[ind][1] = newName
+            for ind, item in enumerate(tbl_SEN_G2_2010):
+                if item[1] == currName:
+                    tbl_SEN_G2_2010[ind][1] = newName
+        # Replace Province
+        shuf_PROV_lst = orig_PROV_lst.copy()
+        random.seed(333)
+        random.shuffle(shuf_PROV_lst)
+        # print(shuf_PROV_lst)
+        for i in range(len(shuf_PROV_lst)):
+            currName = shuf_PROV_lst[i]
+            newName = 'Province ' + str(i + 1)
+            for ind, item in enumerate(tbl_SEN_G1_2010):
+                if item[0] == currName:
+                    tbl_SEN_G1_2010[ind][0] = newName
+        # Replace Facility Location
+        shuf_LOCAT_lst = orig_LOCAT_lst.copy()
+        random.seed(333)
+        random.shuffle(shuf_LOCAT_lst)
+        # print(shuf_LOCAT_lst)
+        for i in range(len(shuf_LOCAT_lst)):
+            currName = shuf_LOCAT_lst[i]
+            newName = 'District ' + str(i + 1)
+            for ind, item in enumerate(tbl_SEN_G2_2010):
+                if item[0] == currName:
+                    tbl_SEN_G2_2010[ind][0] = newName
+        # Swap Districts 7 & 8
+        for ind, item in enumerate(tbl_SEN_G2_2010):
+            if item[0] == 'District 7':
+                tbl_SEN_G2_2010[ind][0] = 'District 8'
+            elif item[0] == 'District 8':
+                tbl_SEN_G2_2010[ind][0] = 'District 7'
+
+    # Now form data dictionary
+    retDict = util.testresultsfiletotable(tbl_SEN_G2_2010, csvName=False)
+    if deidentify == True:
+        retlist_MANUF = shuf_MANUF_lst.copy()
+        retlist_PROV = shuf_PROV_lst.copy()
+        retlist_LOCAT = shuf_LOCAT_lst.copy()
+    else:
+        retlist_MANUF = orig_MANUF_lst.copy()
+        retlist_PROV = orig_PROV_lst.copy()
+        retlist_LOCAT = orig_LOCAT_lst.copy()
+
+    return retDict['N'], retDict['Y'], retlist_MANUF, retlist_PROV, retlist_LOCAT
+
+# Pull data from newly constructed CSV files
+def GetSenegalCSVData():
+    """
+    Travel out-and-back times for districts/departments are expressed as the proportion of a 10-hour workday, and
+    include a 30-minute collection time; traveling to every region outside the HQ region includes a 2.5 hour fixed cost
+    """
+    dept_df = pd.read_csv('operationalizedsamplingplans/senegal_csv_files/deptfixedcosts.csv', header=0)
+    regcost_mat = pd.read_csv('operationalizedsamplingplans/senegal_csv_files/regarcfixedcosts.csv', header=None)
+    regNames = ['Dakar', 'Diourbel', 'Fatick', 'Kaffrine', 'Kaolack', 'Kedougou', 'Kolda', 'Louga', 'Matam',
+                'Saint-Louis', 'Sedhiou', 'Tambacounda', 'Thies', 'Ziguinchor']
+    # Get testing results
+    testresults_df = pd.read_csv('operationalizedsamplingplans/senegal_csv_files/dataresults.csv', header=0)
+    manufNames = testresults_df.Manufacturer.sort_values().unique().tolist()
+
+    return dept_df, regcost_mat, testresults_df, regNames, manufNames
+
+dept_df, regcost_mat, testresults_df, regNames, manufNames = GetSenegalCSVData()
+deptNames = dept_df['Department'].sort_values().tolist()
+numReg = len(regNames)
+testdatadict = {'dataTbl':testresults_df.values.tolist(), 'type':'Tracked', 'TNnames':deptNames, 'SNnames':manufNames}
+testdatadict = util.GetVectorForms(testdatadict)
+N, Y, TNnames, SNnames = testdatadict['N'], testdatadict['Y'], testdatadict['TNnames'], testdatadict['SNnames']
+# Drop all nontested districts
+keepinds = np.where(np.sum(N,axis=1)>0)
+N, Y = N[keepinds], Y[keepinds]
+# Drop supply nodes with less than 5 tests
+keepinds = np.where(np.sum(N, axis=0)>4)
+N, Y = N[:, keepinds[0]], Y[:, keepinds[0]]
+
+(numTN, numSN) = N.shape # (20, 14)
+
+# Set up logistigate dictionary
+lgdict = util.initDataDict(N, Y)
+lgdict.update({'TNnames':TNnames, 'SNnames':SNnames})
+
+# Set up priors
+SNpriorMean = np.repeat(spsp.logit(0.1), numSN)
+# TNs are randomly assigned risk, such that 5% are in the 1st and 7th levels, 10% are in the 2nd and 6th levels,
+#   20% are in the 3rd and 5th levels, and 30% are in the 4th level
+np.random.seed(15)
+tempCategs = np.random.multinomial(n=1, pvals=[0.05,0.1,0.2,0.3,0.2,0.1,0.05], size=numTN)
+riskMeans = [0.01,0.02,0.05,0.1,0.15,0.2,0.25]
+randriskinds = np.mod(np.where(tempCategs.flatten()==1), len(riskMeans))[0]
+TNpriorMean = spsp.logit(np.array([riskMeans[randriskinds[i]] for i in range(numTN)]))
+# Concatenate prior means
+priorMean = np.concatenate((SNpriorMean, TNpriorMean))
+TNvar, SNvar = 2., 3.  # Variances for use with prior; supply nodes are wider due to unknown risk assessments
+priorCovar = np.diag(np.concatenate((np.repeat(SNvar, numSN), np.repeat(TNvar, numTN))))
+priorObj = prior_normal_assort(priorMean, priorCovar)
+lgdict['prior'] = priorObj
+
+# Set up MCMC
+lgdict['MCMCdict'] = {'MCMCtype': 'NUTS', 'Madapt': 5000, 'delta': 0.4}
+
+'''
+numdraws = 20000
+lgdict['numPostSamples'] = numdraws
+
+for i in range(1, 5):
+    np.random.seed(500 + i)
+    print('On draw set', i, '...')
+    time0 = time.time()
+    lgdict = methods.GeneratePostSamples(lgdict, maxTime=5000)
+    print('Time:',time.time()-time0)
+    tempobj = lgdict['postSamples']
+    np.save(os.path.join('studies', 'importancedraws_14MAR24', 'draws'+str(i+1)), tempobj)
+'''
+
+# Load draws from files
+tempobj = np.load(os.path.join('studies', 'importancedraws_14MAR24', 'draws1.npy'))
+for drawgroupind in range(2, 6):
+    newobj = np.load(os.path.join('studies', 'importancedraws_14MAR24', 'draws' + str(drawgroupind) +'.npy'))
+    tempobj = np.concatenate((tempobj, newobj))
+lgdict['postSamples'] = tempobj
+# Print inference from initial data
+util.plotPostSamples(lgdict, 'int90')
+
+# TODO: GENERATE A NEW Q?
+
+# Build parameter dictionary for utility estimation
+paramdict = lf.build_diffscore_checkrisk_dict(scoreunderestwt=5., riskthreshold=0.13, riskslope=0.6,
+                                              marketvec=np.ones(numTN + numSN))
+
+def getUtilityEstimate(n, lgdict, paramdict, zlevel=0.95):
+    """
+    Return a utility estimate average and confidence interval for allocation array n
+    """
+    testnum = int(np.sum(n))
+    des = n/testnum
+    currlosslist = sampf.sampling_plan_loss_list(des, testnum, lgdict, paramdict)
+    currloss_avg, currloss_CI = sampf.process_loss_list(currlosslist, zlevel=zlevel)
+    return paramdict['baseloss'] - currloss_avg, (paramdict['baseloss']-currloss_CI[1], paramdict['baseloss']-currloss_CI[0])
+
+truthdrawslist = [1000, 5000, 10000, 20000, 40000, 80000]
+numdatadraws = 100 # Same for all truth draws
+
+# Do 20 tests at 4 nodes and inspect the utility under different numbers of truth draws
+n = np.zeros(numTN)
+# np.sum(N, axis=0)
+n[15], n[1], n[2], n[3], n[9], n[11], n[16] = 50, 40, 50, 30, 50, 60, 30
+
+util_list, utilCI_list = [], []
+for numtruthdraws in truthdrawslist:
+    np.random.seed(432)
+    truthdraws, datadraws = util.distribute_truthdata_draws(lgdict['postSamples'], numtruthdraws, numdatadraws)
+    paramdict.update({'truthdraws': truthdraws, 'datadraws': datadraws})
+    paramdict['baseloss'] = sampf.baseloss(paramdict['truthdraws'], paramdict) # Get base loss
+    util.print_param_checks(paramdict)  # Check of used parameters
+    time0 = time.time()
+    utilavg, (utilCIlo, utilCIhi) = getUtilityEstimate(n, lgdict, paramdict)
+    util_list.append(utilavg), utilCI_list.append((utilCIlo, utilCIhi))
+    print(time.time() - time0)
+    print(utilavg, (utilCIlo, utilCIhi))
+
+'''18-MAR-24
+util_list = [1.928953581133419, 1.3653450593217058, 1.19049988687896,
+ 1.0274356559783024, 0.9809383681292192, 0.905028881148298]
+utilCI_list = [(1.7511708056790125, 2.1067363565878257), (1.197911050025763, 1.5327790686176486),
+ (1.0437844208013858, 1.3372153529565343), (0.9135623498567207, 1.1413089620998842),
+ (0.8610240620046747, 1.1008526742537637), (0.8048188986619182, 1.0052388636346778)]
+'''
+
+# Plot of estimates under current method
+utilerrs = np.array([utilCI_list[i][1] - util_list[i] for i in range(len(truthdrawslist))])
+plt.errorbar(truthdrawslist, util_list, yerr=utilerrs, ms=15, mfc='red', mew=1, mec='red', capsize=2)
+plt.ylim([0, 2.2])
+plt.xlim([0, 80000])
+plt.title('Utility estimates vs. truth draws\nCurrent method')
+plt.xlabel('Truth Draws')
+plt.ylabel('Utility Estimate')
+plt.show()
+
+#################################
+#################################
+# Now generate new estimation with MCMC importance draws
+#################################
+#################################
+
+def importance_method_loss_list(design, numtests, priordatadict, paramdict, numdatadrawsforimportance, numimportdraws):
+    """
+    Produces a list of sampling plan losses for a test budget under a given data set and specified parameters, using
+    the fast estimation algorithm with direct optimization (instead of a loss matrix).
+    design: sampling probability vector along all test nodes/traces
+    numtests: test budget
+    priordatadict: logistigate data dictionary capturing known data
+    paramdict: parameter dictionary containing a loss matrix, truth and data MCMC draws, and an optional method for
+        rounding the design to an integer allocation
+    """
+    if 'roundalg' in paramdict: # Set default rounding algorithm for plan
+        roundalg = paramdict['roundalg'].copy()
+    else:
+        roundalg = 'lo'
+    # Initialize samples to be drawn from traces, per the design, using a rounding algorithm
+    sampMat = util.generate_sampling_array(design, numtests, roundalg)
+    #todo: KEY NEW STUFF ADDED HERE
+    (numTN, numSN), Q, s, r = priordatadict['N'].shape, priordatadict['Q'], priordatadict['diagSens'], priordatadict['diagSpec']
+    importancedatadrawinds = np.random.choice(np.arange(paramdict['datadraws'].shape[0]),
+                                              size = numdatadrawsforimportance, replace=False)
+    importancedatadraws = paramdict['datadraws'][importancedatadrawinds]
+    numtruthdraws = paramdict['truthdraws'].shape[0]
+    zMatTruth = util.zProbTrVec(numSN, paramdict['truthdraws'], sens=s,
+                                spec=r)  # Matrix of SFP probabilities, as a function of SFP rate draws
+    zMatData = util.zProbTrVec(numSN, importancedatadraws, sens=s, spec=r)  # Probs. using data draws
+    NMat = np.moveaxis(np.array([np.random.multinomial(sampMat[tnInd], Q[tnInd], size=numdatadrawsforimportance)
+                                 for tnInd in range(numTN)]), 1, 0).astype(int)
+    YMat = np.random.binomial(NMat, zMatData)
+    # Get 'average' rounded data set from these few draws
+    NMatAvg, YMatAvg = np.round(np.average(NMat, axis=0)).astype(int), np.round(np.average(YMat, axis=0)).astype(int)
+    # Add these data to a new data dictionary and generate a new set of MCMC draws
+    tempdict = priordatadict.copy()
+    tempdict['N'], tempdict['Y'] = priordatadict['N'] + NMatAvg, priordatadict['Y'] + YMatAvg
+    # Generate a new MCMC importance set
+
+
+
+
+    # Get weights matrix
+    W = sampf.build_weights_matrix(paramdict['truthdraws'], paramdict['datadraws'], sampMat, priordatadict)
+    # Get risk matrix
+    R = lf.risk_check_array(paramdict['truthdraws'], paramdict['riskdict'])
+    # Get critical ratio
+    q = paramdict['scoredict']['underestweight'] / (1 + paramdict['scoredict']['underestweight'])
+    # Compile list of optima
+    minslist = []
+    for j in range(W.shape[1]):
+        est = bayesest_critratio(paramdict['truthdraws'], W[:, j], q)
+        minslist.append(cand_obj_val(est, paramdict['truthdraws'], W[:, j], paramdict, R))
+    return minslist
+
+def getImportanceUtilityEstimate(n, lgdict, paramdict, zlevel=0.95):
+    """
+    Return a utility estimate average and confidence interval for allocation array n, using a second MCMC set of
+    'importance' draws
+    """
+    testnum = int(np.sum(n))
+    des = n/testnum
+    # TODO: FOCUS IS HERE
+    currlosslist = importance_method_loss_list(des, testnum, lgdict, paramdict)
+    # TODO: END OF FOCUS
+    currloss_avg, currloss_CI = sampf.process_loss_list(currlosslist, zlevel=zlevel)
+    return paramdict['baseloss'] - currloss_avg, (paramdict['baseloss']-currloss_CI[1], paramdict['baseloss']-currloss_CI[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
