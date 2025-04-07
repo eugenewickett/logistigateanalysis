07235287771ad2e5b8a66416e2eced97150c9165
Eugene <eugenewickett@gmail.com>
2025-04-07 14:57:43 -0500

    update

--- STUDY_OPsolCorr_3APR25.py
+++ STUDY_OPsolCorr_3APR25.py
@@ -41,7 +41,7 @@ paramdict = lf.build_diffscore_checkrisk_dict(scoreunderestwt=5., riskthreshold=
                                               marketvec=np.ones(numTN + numSN))
 
 # Set up utility estimation parameter dictionary with desired truth and data draws
-SetupUtilEstParamDict(lgdict, paramdict, 300000, 500, randseed=56)
+SetupUtilEstParamDict(lgdict, paramdict, 100000, 300, randseed=56)
 util.print_param_checks(paramdict)  # Parameter check
 
 # Set these parameters per the program described in the paper
@@ -142,7 +142,7 @@ def GetObjective(lvec, m1vec, m2vec, numPath):
 optobjvec = GetObjective(lvec, m1vec, m2vec, numPath)
 
 # Constraints
-def GetConstraints(optparamdict, juncvec, seqcostlist, bindistaccessvectors):
+def GetConstraints(optparamdict, juncvec, seqcostlist, bindistaccessvectors, omegamat):
     numTN, B, ctest = len(optparamdict['deptnames']), optparamdict['budget'], optparamdict['pertestcost']
     f_dept, bigM = optparamdict['deptfixedcostvec'], optparamdict['Mconstant']
     # Build lower and upper inequality values
@@ -154,21 +154,34 @@ def GetConstraints(optparamdict, juncvec, seqcostlist, bindistaccessvectors):
     # Build A matrix, from left to right
     # Build z district binaries first
     optconstraintmat1 = np.vstack((f_dept, -bigM * np.identity(numTN), np.identity(numTN), 0 * np.identity(numTN),
-                                   np.identity(numTN), np.zeros(numTN)))
+                                   np.identity(numTN), np.zeros(numTN),
+                                   0 * np.identity(numTN), 0 * np.identity(numTN), 0 * np.identity(numTN)))
     # n^' matrices
-    optconstraintmat2 = np.vstack((ctest * np.ones(numTN), np.identity(numTN), -np.identity(numTN), np.identity(numTN),
-                                   0 * np.identity(numTN), np.zeros(numTN)))
+    optconstraintmat2 = np.vstack((ctest * np.ones(numTN), np.identity(numTN), -np.identity(numTN),
+                                   np.identity(numTN), 0 * np.identity(numTN), np.zeros(numTN),
+                                   -omegamat, 0 * np.identity(numTN), 0 * np.identity(numTN)))
     # n^'' matrices
     optconstraintmat3 = np.vstack((ctest * np.ones(numTN), np.identity(numTN), -np.identity(numTN),
-                                   0 * np.identity(numTN), 0 * np.identity(numTN), np.zeros(numTN)))
+                                   0 * np.identity(numTN), 0 * np.identity(numTN), np.zeros(numTN),
+                                   -omegamat, 0 * np.identity(numTN), 0 * np.identity(numTN)))
     # path matrices
     optconstraintmat4 = np.vstack((np.array(seqcostlist).T, np.zeros((numTN * 3, numPath)),
-                                   (-bindistaccessvectors).T, np.ones(numPath)))
+                                   (-bindistaccessvectors).T, np.ones(numPath),
+                                    np.zeros((numTN * 3, numPath))))
+    # z_hat matrices
+    optconstraintmat5 = np.vstack((np.zeros(((numTN * 6) + 2, numTN)), np.identity(numTN)))
+    # n_hat^' matrices
+    optconstraintmat6 = np.vstack((np.zeros(((numTN*4)+2, numTN)),
+                                   np.identity(numTN), np.identity(numTN), -np.identity(numTN)))
+    # n_hat^'' matrices
+    optconstraintmat7 = np.vstack((np.zeros(((numTN*4)+2, numTN)),
+                                   np.identity(numTN), 0 * np.identity(numTN), -np.identity(numTN)))
 
-    optconstraintmat = np.hstack((optconstraintmat1, optconstraintmat2, optconstraintmat3, optconstraintmat4))
+    optconstraintmat = np.hstack((optconstraintmat1, optconstraintmat2, optconstraintmat3, optconstraintmat4,
+                                  optconstraintmat5, optconstraintmat6, optconstraintmat7))
     return spo.LinearConstraint(optconstraintmat, optconstrlower, optconstrupper)
 
-optconstraints = GetConstraints(optparamdict, juncvec, seqcostlist_trim, bindistaccessvectors_trim)
+optconstraints = GetConstraints(optparamdict, juncvec, seqcostlist_trim, bindistaccessvectors_trim, omegaMat)
 
 def GetIntegrality(optobjvec):
     return np.ones_like(optobjvec)
@@ -180,12 +193,139 @@ optintegrality = GetIntegrality(optobjvec)
 spoOutput = milp(c=optobjvec, constraints=optconstraints, integrality=optintegrality, bounds=optbounds)
 initsoln, initsoln_obj = spoOutput.x, spoOutput.fun*-1
 
-opf.scipytoallocation(initsoln, deptNames, regNames, seqlist_trim, eliminateZeros=True)
+def scipytoallocation(spo_x, distNames, regNames, seqlist_trim_df, eliminateZeros=False):
+    """function for turning scipy solution into something interpretable"""
+    tnnum = len(distNames)
+    z = np.round(spo_x[:tnnum])
+    n1 = np.round(spo_x[tnnum:tnnum * 2])
+    n2 = np.round(spo_x[tnnum * 2:tnnum * 3])
+    x = np.round(spo_x[tnnum * 3:]) # Solver sometimes gives non-integer solutions
+    path = eval(seqlist_trim_df.iloc[np.where(x == 1)[0][0],0])
+    # Print district name with key solution elements
+    for distind, distname in enumerate(distNames):
+        if not eliminateZeros:
+            print(str(distname)+':', str(int(z[distind])), str(int(n1[distind])), str(int(n2[distind])))
+        else:  # Remove zeros
+            if int(z[distind])==1:
+                print(str(distname)+ ':', str(int(z[distind])), str(int(n1[distind])), str(int(n2[distind])))
+    pathstr = ''
+    for regind in path:
+        pathstr = pathstr + str(regNames[regind]) + ' '
+    print('Path: '+ pathstr)
+    return
 
+scipytoallocation(initsoln, deptNames, regNames, seqlist_trim, eliminateZeros=True)
 
+'''SOLUTION UNDER ORIGINAL IP-RP; 
+Bambey: 1 0 11
+Birkilane: 1 0 8
+Dakar: 1 17 25
+Fatick: 1 0 14
+Foundiougne: 1 0 10
+Gossas: 1 0 9
+Guinguineo: 1 0 11
+Kaffrine: 1 0 11
+Kaolack: 1 3 39
+Keur Massar: 1 0 31
+Koungheul: 1 9 36
+Malem Hoddar: 1 0 10
+Mbacke: 1 0 8
+Nioro du Rip: 1 0 15
+Pikine: 1 0 9
+Path: Dakar Fatick Kaolack Kaffrine Diourbel
 
+util: 5.636652804231508
+util_CI: (5.289598860601801, 5.9837067478612145)
+'''
+def scipysoltoallocvec(spo_x, tnnum):
+    return spo_x[tnnum:tnnum*2] + spo_x[tnnum*2:tnnum*3]
 
+n = scipysoltoallocvec(initsoln, numTN)
+util, util_CI = sampf.getUtilityEstimate_parallel(n, lgdict, paramdict, zlevel=0.95)
+print(util)
+print(util_CI)
 
+########
+# Now update omegaMat and see what happens
+########
+# What is the ratio of importance of learning more about SNs?
+maxratio = 1/2
 
+# Build Jaccard index for each pair of districts
+Qbin = np.ceil(Q)  # binary matrix identifying *any* sourcing between TN and SN
+J = np.zeros((numTN, numTN))
+for i in range(numTN):
+    for j in range(numTN):
+        s1 = np.nonzero(Qbin[i])
+        s2 = np.nonzero(Qbin[j])
+        J[i, j] = np.intersect1d(s1, s2).shape[0] / np.union1d(s1, s2).shape[0]
 
+Jadj = J * maxratio  # adjust for maximum learning importance
+Jadj = Jadj + np.identity(numTN)*(1-maxratio)  # adjust for diagonals to be 1
+omegaMat = Jadj
+
+optconstraints = GetConstraints(optparamdict, juncvec, seqcostlist_trim, bindistaccessvectors_trim, omegaMat)
+spoOutput = milp(c=optobjvec, constraints=optconstraints, integrality=optintegrality, bounds=optbounds)
+initsoln, initsoln_obj = spoOutput.x, spoOutput.fun*-1
+scipytoallocation(initsoln, deptNames, regNames, seqlist_trim, eliminateZeros=True)
+
+''' SOLUTION WITH INITIAL JACCARD INDEX; TOO MUCH VALUE FROM SIMPLY INCREASING TESTS AT REACHABLE LOCATIONS
+Dakar: 1 26 25
+Fatick: 1 14 67
+Foundiougne: 1 10 71
+Gossas: 1 9 72
+Guediawaye: 1 51 30
+Keur Massar: 1 31 50
+Pikine: 1 9 72
+Path: Dakar Fatick
+util: 6.521669243524179
+util_CI: (6.158360526804159, 6.884977960244199)
+'''
+n = scipysoltoallocvec(initsoln, numTN)
+util, util_CI = sampf.getUtilityEstimate_parallel(n, lgdict, paramdict, zlevel=0.95)
+print(util)
+print(util_CI)
+
+### TRY Q NORM
+QnormMat = np.zeros((numTN, numTN))
+for i in range(numTN):
+    for j in range(numTN):
+        QnormMat[i, j] = maxratio * (1 - (np.linalg.norm(Q[i]-Q[j])/np.sqrt(2)))
+
+QnormMat = QnormMat + np.identity(numTN)*(1-maxratio)  # adjust for diagonals to be 1
+
+omegaMat = QnormMat
+
+optconstraints = GetConstraints(optparamdict, juncvec, seqcostlist_trim, bindistaccessvectors_trim, omegaMat)
+spoOutput = milp(c=optobjvec, constraints=optconstraints, integrality=optintegrality, bounds=optbounds)
+initsoln, initsoln_obj = spoOutput.x, spoOutput.fun*-1
+scipytoallocation(initsoln, deptNames, regNames, seqlist_trim, eliminateZeros=True)
+
+'''
+Bambey: 1 9 70
+Dakar: 1 55 25
+Diourbel: 1 77 4
+Guediawaye: 1 50 30
+Keur Massar: 1 11 50
+Mbacke: 1 8 73
+Pikine: 1 8 72
+Path: Dakar Diourbel
+ 
+'''
+n = scipysoltoallocvec(initsoln, numTN)
+util, util_CI = sampf.getUtilityEstimate_parallel(n, lgdict, paramdict, zlevel=0.95)
+
+# What if correlation value is too high? Try Jaccard index with a smaller maxratio
+maxratio = 1/100
+Jadj = J * maxratio  # adjust for maximum learning importance
+Jadj = Jadj + np.identity(numTN)*(1-maxratio)  # adjust for diagonals to be 1
+omegaMat = Jadj
+
+optconstraints = GetConstraints(optparamdict, juncvec, seqcostlist_trim, bindistaccessvectors_trim, omegaMat)
+spoOutput = milp(c=optobjvec, constraints=optconstraints, integrality=optintegrality, bounds=optbounds)
+initsoln, initsoln_obj = spoOutput.x, spoOutput.fun*-1
+scipytoallocation(initsoln, deptNames, regNames, seqlist_trim, eliminateZeros=True)
+
+n = scipysoltoallocvec(initsoln, numTN)
+util, util_CI = sampf.getUtilityEstimate_parallel(n, lgdict, paramdict, zlevel=0.95)
 
